6c27c0b7bd42cb6b412942df4fe3c81ce3c999aa
Arthur Detomi <arthurdetomi@gmail.com>
2024-01-16 20:34:11 -0300

    Adicionando arquivos

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React!</h1>
+      <h2>Emmet Funcionou!</h2>
+
+      <h3>Author: G. Arthur Detomi</h3>
+
+
+    </div>
+  );
+}
+
+export default App;
