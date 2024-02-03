6e622f09bde0da41cd24b7c226d8afa56221daeb
Arthur Detomi <arthurdetomi@gmail.com>
2024-02-03 15:39:52 -0300

    A prop children

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -5,6 +5,7 @@ import ConditionalRender from "./components/ConditionalRender"
 import ShowUserName from "./components/ShowUserName"
 import CarDetails from "./components/CarDetails"
 import Fragment from "./components/Fragment"
+import Container from "./components/Container"
 
 import { useState } from "react"
 
@@ -16,9 +17,9 @@ function App() {
   const [userName] = useState("Maria");
 
   const cars = [
-    {id:1, brand: "Ferrari", color:"Amarela", newCar: true, km: 0},
-    {id:2, brand: "Kia", color:"Branco", newCar: false, km: 0},
-    {id:3, brand: "Renault", color:"Azul", newCar: true, km: 0}
+    { id: 1, brand: "Ferrari", color: "Amarela", newCar: true, km: 0 },
+    { id: 2, brand: "Kia", color: "Branco", newCar: false, km: 0 },
+    { id: 3, brand: "Renault", color: "Azul", newCar: true, km: 0 }
   ];
 
   return (
@@ -34,24 +35,28 @@ function App() {
         <img src={City} alt="Cidade" />
       </div>
 
-      <ManageData/>
-      <ListRender/>
-      <ConditionalRender/>
+      <ManageData />
+      <ListRender />
+      <ConditionalRender />
       {/* props */}
       <ShowUserName name={userName} />
       {/* destructuring properties */}
-      <CarDetails  brand="VW" km={100000} color="Azul" newCar={true} />
+      <CarDetails brand="VW" km={100000} color="Azul" newCar={true} />
       {/* Reaproveitando */}
-      <CarDetails brand="Ford" color="Vermelha" km={0} newCar={false}/>
-      <CarDetails brand="Fiat" color="Prata" km={4500} newCar={false}/>
+      <CarDetails brand="Ford" color="Vermelha" km={0} newCar={false} />
+      <CarDetails brand="Fiat" color="Prata" km={4500} newCar={false} />
       {/* Loop em array de objetos */}
       {
         cars.map((car) => (
-          <CarDetails  brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+          <CarDetails brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
         ))
       }
       {/* Fragment */}
-      <Fragment propFragment="Teste"/>
+      <Fragment propFragment="Teste" />
+      {/* Children */}
+      <Container myValue="Teste">
+        <p>Este é o conteudo</p>
+      </Container>
     </div>
   );
 }
