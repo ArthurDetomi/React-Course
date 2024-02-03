f69a43036e81e4f59b38c687504c3e1fa1a469ba
Arthur Detomi <arthurdetomi@gmail.com>
2024-02-03 15:17:40 -0300

    Renderizando lista de componentes

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -15,6 +15,12 @@ function App() {
 
   const [userName] = useState("Maria");
 
+  const cars = [
+    {id:1, brand: "Ferrari", color:"Amarela", newCar: true, km: 0},
+    {id:2, brand: "Kia", color:"Branco", newCar: false, km: 0},
+    {id:3, brand: "Renault", color:"Azul", newCar: true, km: 0}
+  ];
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -38,6 +44,12 @@ function App() {
       {/* Reaproveitando */}
       <CarDetails brand="Ford" color="Vermelha" km={0} newCar={false}/>
       <CarDetails brand="Fiat" color="Prata" km={4500} newCar={false}/>
+      {/* Loop em array de objetos */}
+      {
+        cars.map((car) => (
+          <CarDetails  brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+        ))
+      }
     </div>
   );
 }
