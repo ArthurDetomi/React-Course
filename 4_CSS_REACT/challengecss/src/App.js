01f3a852f4c8a0fc7800d36748c54e428dc024fd
Arthur Detomi <arthurdetomi@gmail.com>
2024-02-05 22:45:59 -0300

    React e css desafio

--- 4_CSS_REACT/challengecss/src/App.js
+++ 4_CSS_REACT/challengecss/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import CarDetails from './components/CarDetails';
 import './App.css';
 
+import FerrariImg from "./assets/ferrari.jpeg"
+import UnoImg from "./assets/uno.jpeg"
+import CorsaImg from "./assets/corsa.jpeg"
+
 function App() {
+  const cars = [
+    { id: 1, name: "Ferrari", model: "novo", imgUrl: FerrariImg , year:2019 },
+    { id: 2, name: "Uno", model: "novo", imgUrl: UnoImg , year:2020 },
+    { id: 3, name: "Corsa", model: "novo", imgUrl: CorsaImg, year:2022 }
+  ]
+
   return (
     <div className="App">
-        
+      {
+        cars.map((car) => (
+          <CarDetails key={car.id} name={car.name} model={car.model} imgUrl={car.imgUrl} year={car.year}/>
+        ))
+      }
     </div>
   );
 }
--- 4_CSS_REACT/challengecss/src/components/CarDetails.js
+++ 4_CSS_REACT/challengecss/src/components/CarDetails.js
@@ -1,14 +1,17 @@
+import styles from "./CarDetails.module.css"
+
 const CarDetails = ({ name, model, year, imgUrl }) => {
+    const styleClass = `${styles[name.toLowerCase()]}`;
+
     return (
-        <div>
+        <div className={styleClass}>
             <h2>Carro Detalhe:</h2>
             <ul>
                 <li>Nome: {name}</li>
                 <li>Model: {model}</li>
                 <li>Ano: {year}</li>
                 <li>
-                    <img src={imgUrl} alt='Carro ${name}' />
-
+                    <img src={imgUrl} alt={`Car ${name}`} />
                 </li>
             </ul>
         </div>
