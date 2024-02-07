5c6303b9b7bac3487728bd2a143b418e7d16f94e
Arthur Detomi <arthurdetomi@gmail.com>
2024-02-06 21:03:27 -0300

    🔁 Formulário com react

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,18 +1,46 @@
 import "./MyForm.css"
 
+import { useState } from "react"
+
 const MyForm = () => {
+    {/* 3 - Gerenciamento de dados */ }
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Enviar o formulário");
+        console.log(name, email);
+    }
+
     return (
         <div>
-            {/* Criação de form */}
-            <form>
+            {/* 5 - Envio de formulario */}
+            {/* 1 - Criação de form */}
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder="Digite o seu nome" />
+                    <input
+                        onChange={handleName}
+                        type="text"
+                        name="name"
+                        placeholder="Digite o seu nome"
+                    />
                 </div>
                 {/* 2- Label envolvendo input */}
                 <label>
                     <span>E-mail:</span>
-                    <input type="email" name="email" placeholder="Digite o seu e-mail:" />
+                    {/* 4 - Simplificação de manipulação de state */}
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        name="email"
+                        placeholder="Digite o seu e-mail:"
+                    />
                 </label>
                 <input type="submit" value="Enviar" />
             </form>
