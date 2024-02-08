6b6d09ec60faee75e568baa6c78ea335bbc4704a
Arthur Detomi <arthurdetomi@gmail.com>
2024-02-07 21:17:33 -0300

    🏃 Projeto secret Word

---
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -0,0 +1,12 @@
+import StartScreen from './components/StartScreen';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <StartScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ 6_SECRET_WORD/secretword/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = () => {
+    return (
+        <div className="start" >
+            <h1>Secret Word</h1>
+            <p>Clique no botão abaixo para começar a jogar</p>
+            <button>Começar jogo</button>
+        </div>
+    );
+}
+
+export default StartScreen;
