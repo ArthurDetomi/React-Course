331e5e6387b8577be33e14533bec4dc7d1cc2b27
Arthur Detomi <arthurdetomi@gmail.com>
2024-02-11 13:32:45 -0300

    Requisições Http com React

---
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      
+    </div>
+  );
+}
+
+export default App;
