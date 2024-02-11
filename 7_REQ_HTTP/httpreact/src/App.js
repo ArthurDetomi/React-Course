b73591d8bf66f52f85bd66d358942d044d29dfa5
Arthur Detomi <arthurdetomi@gmail.com>
2024-02-11 14:59:35 -0300

    Requisições HTPP

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react'
+
+const URL = "http://localhost:3000/products"
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1- resgatando dados com uma chamada sincrona
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(URL);
+      const data = await res.json();
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
-      
+      <ul>
+        {
+          products.map(
+            (product) => (
+              <li key={product.id}>{product.name} - R$ {product.price}</li>
+            )
+          )
+        }
+      </ul>
     </div>
   );
 }
